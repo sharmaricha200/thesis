bcd3847aee7ba7a50f2aa08ef77e39de528d7be9
sharmaricha200 <sharmaricha200@gmail.com>
2019-04-21 22:31:50 -0700

    Add utils directory
    Add DataParser utility to parse the data present in data directory.
    
    Signed-off-by: sharmaricha200 <sharmaricha200@gmail.com>

---
+++ utils/DataParser.py
@@ -0,0 +1,109 @@
+import re
+import collections
+import numpy as np
+import os
+from os.path import isfile, join
+
+MAX_X = 801
+class DataParser:
+    def __init__(self, rootDataDir):
+        self.data = []
+        self.ROOT_DATA_DIR = rootDataDir
+
+    def parseData(self):
+        dirs = next(os.walk(self.ROOT_DATA_DIR))[1]
+        for dir in dirs:
+            currPath = os.path.join(self.ROOT_DATA_DIR, dir)
+            hitsPath = os.path.join(currPath, "hits")
+            sampleFile = os.path.join(currPath, "peak_true.msp")
+            hitsFiles = next(os.walk(hitsPath))[2]
+            hits = {}
+            for hitsFile in hitsFiles:
+                path = os.path.join(hitsPath, hitsFile)
+                name, mzdata = self.parseHitsFile(path)
+                hits[name] = mzdata
+            self.data.append({'hits': hits})
+            self.data.append({'sample':self.parseSampleFile(sampleFile)})
+            print(self.data)
+
+    def parseSampleFile(self, filename):
+        name_re = re.compile('NAME: (.*)')
+        peaks_re = re.compile('Num Peaks: ([0-9]*)')
+        state = 0
+        ret = {}
+        with open(filename, 'r') as f:
+            for line in f:
+                if state == 0:
+                    mz_data = {}
+                    name_match = name_re.match(line)
+                    if name_match is None:
+                        print('File format error on file: ' + filename)
+                        raise Exception
+                    name = name_match.group(1)
+                    state = 1
+                    continue
+                if state == 1:
+                    peaks_match = peaks_re.match(line)
+                    if peaks_match is None:
+                        print('File format error on file: ' + filename)
+                        # TODO: Make sure this exception is handled
+                        raise Exception
+                    else:
+                        mz_data['peaks'] = int(peaks_match.group(1))
+                    mz_data['spectrum'] = np.zeros((MAX_X,))
+                    state = 2
+                    continue
+                if state == 2:
+                    if not line.strip():
+                        ret[name] = mz_data;
+                        state = 0
+                    else:
+                        for pair in line.split(';'):
+                            data = pair.split()
+                            if data:
+                                index = int(data[0])
+                                value = int(data[1])
+                                mz_data['spectrum'][index] = value
+                    continue
+        return ret
+
+    def parseHitsFile(self, filename):
+        try:
+            msp = open(filename, "r")
+        except Exception as err:
+            print("Error opening file: " + filename)
+            raise err
+        else:
+            msp_line_list = msp.readlines()
+            msp.close()
+            mz_data = {}
+
+            mz_data['spectrum'] = np.zeros((MAX_X,))
+            name_re = re.compile('NAME: Library Hit - similarity ([0-9]*), \"(.*)\"')
+            name_match = name_re.match(msp_line_list[0])
+            if name_match is None:
+                print('File format error on file: ' + filename)
+                raise Exception
+            mz_data['score'] = int(name_match.group(1))
+            name = name_match.group(2)
+
+        peaks_re = re.compile('Num Peaks: ([0-9]*)')
+        peaks_match = peaks_re.match(msp_line_list[1])
+        if peaks_match is None:
+            print('File format error on file: ' + filename)
+            # TODO: Make sure this exception is handled
+            raise Exception
+        else:
+            mz_data['peaks'] = int(peaks_match.group(1))
+            for line in msp_line_list[2:]:
+                for pair in line.split(';'):
+                    data = pair.split()
+                    if data:
+                        index = int(data[0])
+                        value = int(data[1])
+                        mz_data['spectrum'][index] = value
+            return name, mz_data
+
+parser = DataParser("/Users/vinay/thesis/data/")
+parser.parseData()
+
