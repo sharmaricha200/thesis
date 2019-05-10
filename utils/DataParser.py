92cb17cb5e4bef3d56a3ecdc56bda2ea8a4880aa
sharmaricha200 <sharmaricha200@gmail.com>
2019-05-09 22:55:25 -0700

    Make some functions private
    Also strip the lines read
    
    Signed-off-by: sharmaricha200 <sharmaricha200@gmail.com>

--- utils/DataParser.py
+++ utils/DataParser.py
@@ -20,19 +20,19 @@ class DataParser:
             hits = {}
             for hitsFile in hitsFiles:
                 path = os.path.join(hitsPath, hitsFile)
-                name, mzdata = self.parseHitsFile(path)
+                name, mzdata = self.__parseHitsFile(path)
                 hits[name] = mzdata
-            self.data.append({'hits': hits})
-            self.data.append({'sample':self.parseSampleFile(sampleFile)})
-            print(self.data)
+            self.data.append({'hits': hits, 'sample':self.__parseSampleFile(sampleFile)})
+            return self.data
 
-    def parseSampleFile(self, filename):
+    def __parseSampleFile(self, filename):
         name_re = re.compile('NAME: (.*)')
         peaks_re = re.compile('Num Peaks: ([0-9]*)')
         state = 0
         ret = {}
         with open(filename, 'r') as f:
-            for line in f:
+            for l in f:
+                line = l.strip()
                 if state == 0:
                     mz_data = {}
                     name_match = name_re.match(line)
@@ -54,7 +54,7 @@ class DataParser:
                     state = 2
                     continue
                 if state == 2:
-                    if not line.strip():
+                    if not line:
                         ret[name] = mz_data;
                         state = 0
                     else:
@@ -67,7 +67,7 @@ class DataParser:
                     continue
         return ret
 
-    def parseHitsFile(self, filename):
+    def __parseHitsFile(self, filename):
         try:
             msp = open(filename, "r")
         except Exception as err:
@@ -95,7 +95,8 @@ class DataParser:
             raise Exception
         else:
             mz_data['peaks'] = int(peaks_match.group(1))
-            for line in msp_line_list[2:]:
+            for l in msp_line_list[2:]:
+                line = l.strip()
                 for pair in line.split(';'):
                     data = pair.split()
                     if data:
@@ -103,7 +104,4 @@ class DataParser:
                         value = int(data[1])
                         mz_data['spectrum'][index] = value
             return name, mz_data
-
-parser = DataParser("/Users/vinay/thesis/data/")
-parser.parseData()
 
