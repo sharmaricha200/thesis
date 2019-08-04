b085d6aae1f343838ae13d822dec955d55268289
sharmaricha200 <sharmaricha200@gmail.com>
2019-08-04 12:15:36 -0700

    Restructure and clean run.py
    Signed-off-by: sharmaricha200 <sharmaricha200@gmail.com>

--- run.py
+++ run.py
@@ -25,20 +25,28 @@ sys.path.insert(0, ROOT_PATH + "/model/ml")
 sys.path.insert(0, ROOT_PATH + "/utils")
 
 import DataParser as dp
-import AlgoModel as am
 import ReportGenerator as rg
-import MLModel as ml
 import numpy as np
 
-#for s in sample:
-#    name = s['name']
-#    if name in hits:
-#        h = hits[name]
-#        percent, molecular_ion, top_three_ion = model.predict(name, h, s)
-#        rp.report("sample1", name, percent, molecular_ion, top_three_ion)
-#    else:
-#        print("This compound is not present in lib hits. Please check input files. Compound: " + name)
+def get_valid_gt(in_gt, sample):
+    out_gt = []
+    for (peak_num, name, confidence) in in_gt:
+        if name in hits and sample[peak_num - 1]['name'] == name:
+            out_gt.append((peak_num, name, confidence))
+    return out_gt
 
+def translate_pred(pred):
+    ret  = []
+    pred[pred <= 0.5] = 0
+    pred[pred > 0.5] = 1
+    for p in pred:
+        if (p[0] == 1 and p[1] == 0):
+            ret.append(1)
+        elif (p[0] == 0 and p[1] == 1):
+            ret.append(2)
+        else:
+            ret.append(-1)
+    return ret
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='1.0')
@@ -51,27 +59,27 @@ if __name__ == '__main__':
     i = 0
     m = 1
     if args['ml']:
+        import MLModel as ml
         dnn = ml.DNNModel(args['-s'])
         if args['train']:
             ground_truth = data[0]['truth_train']
         elif args['test']:
             ground_truth = data[0]['truth_test']
 
-        dt = np.empty([len(ground_truth), 1602])
-        gt = np.empty([len(ground_truth), 2])
-        for (peak_num, name, confidence) in ground_truth:
-            if name in hits and sample[peak_num - 1]['name'] == name:
-                dt[i] = np.concatenate((sample[peak_num - 1]['spectrum'], hits[name]['spectrum']))
-                if confidence == 2:
-                    gt[i][0] = 0
-                    gt[i][1] = 1
-                elif confidence == 1:
-                    gt[i][0] = 1
-                    gt[i][1] = 0
-                i = i + 1
-            else:
-                #print("Name not in hits: " + name)
-                pass
+        valid_gt = get_valid_gt(ground_truth, sample)
+        dt = np.empty([len(valid_gt), 1602])
+        gt = np.empty([len(valid_gt), 2])
+
+        for (peak_num, name, confidence) in valid_gt:
+            dt[i] = np.concatenate((sample[peak_num - 1]['spectrum'], hits[name]['spectrum']))
+            if confidence == 2:
+                gt[i][0] = 0
+                gt[i][1] = 1
+            elif confidence == 1:
+                gt[i][0] = 1
+                gt[i][1] = 0
+            i = i + 1
+
         x = dt[:i]
         y = gt[:i]
         if args['train']:
@@ -79,37 +87,31 @@ if __name__ == '__main__':
         elif args['test']:
             dnn.load()
             dnn.evaluate(x, y)
-            pred = dnn.predict(x)
-            pred[pred <= 0.5] = 0
-            pred[pred > 0.5] = 1
-            print("Test--->")
-            print(y)
-            print("Prediction--->")
-            print(pred)
+            prediction = dnn.predict(x)
+            pred = translate_pred(prediction)
+            i = 0
+            for (peak_num, name, confidence) in valid_gt:
+                print("{peak_num};{name};{confidence};{pred}".format(peak_num=peak_num, name=name, confidence=confidence,pred=pred[i]))
+                i = i + 1
 
     elif args['algo']:
+        import AlgoModel as am
         model = am.AlgoModel(int(args['--st']), int(args['--pt']))
         pred = []
         gt_conf = []
-        ground_truth = data[0]['truth_train']
-        for (peak_num, name, confidence) in ground_truth:
-            if name in hits and sample[peak_num - 1]['name'] == name:
-                conf, percentage_lib_hit_present, molecular_ion_hit, top_three_ion_list_sample = model.predict(hits[name], sample[peak_num - 1])
-                pred.append(conf)
-                gt_conf.append(confidence)
-        ground_truth = data[0]['truth_test']
-        for (peak_num, name, confidence) in ground_truth:
-            if name in hits and sample[peak_num - 1]['name'] == name:
-                conf, percentage_lib_hit_present, molecular_ion_hit, top_three_ion_list_sample = model.predict(hits[name], sample[peak_num - 1])
-                pred.append(conf)
-                gt_conf.append(confidence)
+        ground_truth = [*data[0]['truth_train'], *data[0]['truth_test']]
+        valid_gt = get_valid_gt(ground_truth, sample)
+        for (peak_num, name, confidence) in valid_gt:
+            conf, percentage_lib_hit_present, molecular_ion_hit, top_three_ion_list_sample = model.predict(hits[name], sample[peak_num - 1])
+            pred.append(conf)
+            gt_conf.append(confidence)
         pred = np.array(pred)
         gt_conf = np.array(gt_conf)
-        print("Test -->")
-        print(gt_conf)
         right = pred == gt_conf
-        print(np.sum(right) / right.size * 100)
-        print("Prediction --->")
+        accuracy = np.sum(right) / right.size * 100
+        print("acc = {acc}%".format(acc=accuracy))
+        i = 0
+        for (peak_num, name, confidence) in valid_gt:
+            print("{peak_num};{name};{confidence};{pred}".format(peak_num=peak_num, name=name, confidence=confidence,pred=pred[i]))
+            i = i+1
         print(pred)
-
-
