628617cc0c69e1fe0c612dbf9d08a5e3fc459142
sharmaricha200 <sharmaricha200@gmail.com>
2019-08-13 21:55:13 -0700

    Improve algorithmic model
    Signed-off-by: sharmaricha200 <sharmaricha200@gmail.com>

--- model/algorithmic/AlgoModel.py
+++ model/algorithmic/AlgoModel.py
@@ -1,21 +1,27 @@
 import numpy as np
 
+NOISE_THRESHOLD = 0.02
+PERCENT_HIGH = 80
+PERCENT_MED = 55
+TOP_N = 3
 class AlgoModel:
     def __init__(self, similarity_th, percent_th):
         self.SIMILARITY_THRESHOLD = similarity_th
         self.PERCENT_THRESHOLD = percent_th
+       # print("Configuration: {phigh}%, {pmed}%, {noise}, {topn}".
+       #       format(phigh=PERCENT_HIGH, pmed=PERCENT_MED, noise=NOISE_THRESHOLD, topn=TOP_N))
         pass
 
     def __get_top_three(self, mz_intensity):
-        ind = np.argpartition(mz_intensity, -3)[-3:]
+        ind = np.argpartition(mz_intensity, -TOP_N)[-TOP_N:]
         return ind[np.argsort(mz_intensity[ind])]
 
     def __get_molecular_ion(self, mz_intensity):
         return mz_intensity[0][-1]
 
     def __get_percentage_lib_hit(self, mz_intensity_compd, mz_intensity_lib):
-        mz_compd_list = np.where(mz_intensity_compd >= 20)
-        mz_lib_list = np.where(mz_intensity_lib >= 20)
+        mz_compd_list = np.where(mz_intensity_compd >= NOISE_THRESHOLD)
+        mz_lib_list = np.where(mz_intensity_lib >= NOISE_THRESHOLD)
         compd_in_lib = np.isin(mz_compd_list, mz_lib_list).sum()
         lib_in_compd = np.isin(mz_lib_list, mz_compd_list).sum()
         percentage_compd_in_lib_hit_present = compd_in_lib * 100 / len(mz_compd_list[0])
@@ -31,22 +37,28 @@ class AlgoModel:
         if score >= self.SIMILARITY_THRESHOLD:
             sample_spectrum = sample['spectrum']
             hit_spectrum = hit['spectrum']
+            sample_spectrum = sample_spectrum / np.linalg.norm(sample_spectrum)
+            hit_spectrum = hit_spectrum/np.linalg.norm(hit_spectrum)
             hit_spectrum[0:49] = 0
             top_three_ion_list_sample = self.__get_top_three(sample_spectrum)
             top_three_ion_list_hit = self.__get_top_three(hit_spectrum)
-            hit_list = np.where(hit_spectrum >= 20)
-            sample_list = np.where(sample_spectrum >= 20)
+            hit_list = np.where(hit_spectrum >= NOISE_THRESHOLD)
+            sample_list = np.where(sample_spectrum >= NOISE_THRESHOLD)
             molecular_ion_hit = self.__get_molecular_ion(hit_list)
+            molecular_ion_sample = self.__get_molecular_ion(sample_list)
             percentage_lib_hit_present = self.__get_percentage_lib_hit(sample_spectrum, hit_spectrum)
-            if np.isin(top_three_ion_list_hit, sample_list).sum() >= 2:
-                if np.isin(molecular_ion_hit, sample_list).sum() > 0:
-                    if percentage_lib_hit_present >= self.PERCENT_THRESHOLD:
-                        confidence = 2
-                    else:
-                        confidence = 1
-                else:
-                    confidence = 1
-        else:
-            confidence = 0
+            dot_product = np.dot(sample_spectrum, hit_spectrum) * 100
+            if (np.isin(top_three_ion_list_hit, sample_list).sum() >= TOP_N
+                and np.isin(molecular_ion_hit, sample_list).sum() > 0
+                and np.isin(top_three_ion_list_sample, hit_list).sum() >= TOP_N
+                and dot_product >= PERCENT_HIGH):
+                confidence = 2
+            elif(((np.isin(top_three_ion_list_hit, sample_list).sum() >= TOP_N
+                  or np.isin(top_three_ion_list_sample, hit_list).sum() >= TOP_N)
+                  and np.isin(molecular_ion_hit, sample_list).sum() > 0
+                and dot_product >= PERCENT_MED)):
+                confidence = 1
+            else:
+                confidence = 0
 
         return confidence, percentage_lib_hit_present, molecular_ion_hit, top_three_ion_list_sample
