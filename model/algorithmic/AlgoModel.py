9524684a3c4371162edc740bf01568881f6a7c73
jbendik14 <46578752+jbendik14@users.noreply.github.com>
2021-07-15 16:35:12 -0700

    Add files via upload

--- model/algorithmic/AlgoModel.py
+++ model/algorithmic/AlgoModel.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import numpy as np
+import math
 
 NOISE_THRESHOLD = 0.02
 PERCENT_HIGH = 80
@@ -19,15 +21,15 @@ class AlgoModel:
         return mz_intensity[0][-1]
 
     def __get_percentage_lib_hit(self, mz_intensity_compd, mz_intensity_lib):
-        mz_compd_list = np.where(mz_intensity_compd >= NOISE_THRESHOLD)
-        mz_lib_list = np.where(mz_intensity_lib >= NOISE_THRESHOLD)
+        mz_compd_list = np.where(mz_intensity_compd > NOISE_THRESHOLD)
+        mz_lib_list = np.where(mz_intensity_lib > NOISE_THRESHOLD)
         compd_in_lib = np.isin(mz_compd_list, mz_lib_list).sum()
         lib_in_compd = np.isin(mz_lib_list, mz_compd_list).sum()
         percentage_compd_in_lib_hit_present = compd_in_lib * 100 / len(mz_compd_list[0])
         percentage_lib_hit_in_compd_present = lib_in_compd * 100 / len(mz_lib_list[0])
         return max(percentage_compd_in_lib_hit_present, percentage_lib_hit_in_compd_present)
 
-    def predict(self, hit, sample):
+    def predict(self, hit, sample, molecular_ion):
         score = hit['score']
         confidence = 0
         percentage_lib_hit_present = 0
@@ -43,16 +45,14 @@ class AlgoModel:
             top_n_ion_list_hit = self.__get_top_n(hit_spectrum)
             hit_list = np.where(hit_spectrum >= NOISE_THRESHOLD)
             sample_list = np.where(sample_spectrum >= NOISE_THRESHOLD)
-            molecular_ion_hit = self.__get_molecular_ion(hit_list)
-            molecular_ion_sample = self.__get_molecular_ion(sample_list)
             percentage_lib_hit_present = self.__get_percentage_lib_hit(sample_spectrum, hit_spectrum)
             dot_product = np.dot(sample_spectrum, hit_spectrum) * 100
             if (np.isin(top_n_ion_list_hit, sample_list).sum() >= TOP_N
-                and np.isin(molecular_ion_hit, sample_list).sum() > 0
+                and np.isin(molecular_ion-1, sample_list).sum() > 0
                 and np.isin(top_n_ion_list_sample, hit_list).sum() >= TOP_N
                 and dot_product >= PERCENT_HIGH):
                 confidence = 2
             else:
                 confidence = 0
 
-        return confidence, percentage_lib_hit_present, molecular_ion_hit, top_n_ion_list_sample
+        return confidence, percentage_lib_hit_present, molecular_ion - 1, top_n_ion_list_sample
